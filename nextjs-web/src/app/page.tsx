8b8f3edcd4572b077110b287fdf05366c490b73e
Madison Dickson <madison@builder.io>
2024-06-18 08:56:06 -0700

    cleanup styles

--- nextjs-web/src/app/page.tsx
+++ nextjs-web/src/app/page.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-sm lg:flex">
+    <main className="min-h-screen flex-row p-24">
+      <div className="">
         <div className="flex">
-          <h1>Next.js Builder Example</h1>
-          <ul>
+          <h1 className="text-lg">Next.js Builder Example</h1>
+          {/* <ul>
             <li>
               <Link href="category/category-123">Example Category</Link>
             </li>
             <li>
               <Link href="product/product-123">Example Product</Link>
             </li>
-          </ul>
+          </ul> */}
         </div>
 
         <BuilderContentComponent />
--- react-native/app/(tabs)/index.tsx
+++ react-native/app/(tabs)/index.tsx
@@ -1,19 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 // import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
-import { BuilderContentComponent } from '../../components/builder/BuilderContentComponent';
-import { Link } from 'expo-router';
+import { Text, View } from "../../components/Themed";
+import { BuilderContentComponent } from "../../components/builder/BuilderContentComponent";
+import { Link } from "expo-router";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       {/* <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
-      <Link href={'/category/category-123'}>Example Category</Link>
-      <Link href={'/product/product-123'}>Example Product</Link>
-      <BuilderContentComponent/>
+      {/* <Link href={'/category/category-123'}>Example Category</Link> */}
+      {/* <Link href={'/product/product-123'}>Example Product</Link> */}
+      <BuilderContentComponent />
     </View>
   );
 }
@@ -21,16 +25,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
